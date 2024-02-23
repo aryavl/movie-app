2508ec79a6948e4ecdb5bb5e9d963949c7d0b5f3
arya <aryavl9813@gmail.com>
2024-02-24 01:18:37 +0530

    Updated navbar and banner

--- client/components/Banner.tsx
+++ client/components/Banner.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import styles from "./Banner.module.css";
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 const Banner = () => {
   return (
     <div className="relative">
       <div className={`relative ${styles.background}`}>
-        {/* <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-white text-4xl font-bold">Welcome to My Website</h1>
-        </div> */}
         <div className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-[#141414] to-transparent"></div>
       </div>
-      <div className="relative z-10 bg-[#141414] pb-8 pt-2">
-      <div className=" w-[80%] mx-auto overflow-hidden flex items-center justify-center flex-col gap-5 xl:gap-8">
-        <h1 className="text-white mt-5  text-xl sm:text-xxl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-center">Best viewing experience with Utsav</h1>
+      <div className="relative z-10 h-[50vh] sm:h-[35vh] md:h-[40vh] lg:h-[50vh]  bg-[#141414] pb-10 pt-2">
+        <div className=" w-[80%] mx-auto overflow-hidden flex items-center justify-center flex-col gap-5 xl:gap-8"></div>
+      </div>
+      <div className="w-[80%] text-center flex flex-col items-center absolute top-[40%] sm:top-[50%]  md:top-[55%] lg:top-[50%] left-[10%] z-[10] justify-center mx-auto ">
+        <h1 className="text-white mt-5  text-xl sm:text-xxl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-center">
+          Best viewing experience with Utsav
+        </h1>
 
-        <p className=" text-gray-400   text-center ">
-          Utsav is the best streaming experience for watching your favourite movies and shows on demand, anytime, anywhere.
+        <p className=" text-gray-400 mt-5  text-center ">
+          Utsav is the best streaming experience for watching your favourite
+          movies and shows on demand, anytime, anywhere. With Utsav, you can enjoy a wide variety of content, including the latest blockbusters, classic movies, popular TV shows, and more. You can also create your own watchlists, so you can easily find the content you want to watch.
         </p>
 
-        <button className="flex items-center gap-1 bg-red-700 px-4 py-3 rounded-lg text-white mt-2">
-            <PlayArrowIcon/>
-            Start watching now
+        <button className="flex items-center gap-1 mt-5  bg-red-700 px-4 py-3 rounded-lg text-white ">
+          <PlayArrowIcon />
+          Start watching now
         </button>
-      </div>
       </div>
     </div>
   );
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -6,8 +6,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { CloseOutlined } from "@mui/icons-material";
-import { AnimatePresence, motion } from "framer-motion";
-
+import { AnimatePresence,motion } from "framer-motion";
 const Navbar = () => {
   const [open, setOpen] = useState<boolean>(false);
   const navAnimation = open ? "translate-x-0" : "translate-x-[-100%]";
@@ -44,6 +43,8 @@ const Navbar = () => {
             aria-expanded="false"
             onClick={() => setOpen(true)}
           >
+            {/* <span className="sr-only">Open main menu</span> */}
+
             <MenuOpenIcon
               aria-hidden="true"
               className="w-[2rem] h-[2rem] text-white"
@@ -51,22 +52,18 @@ const Navbar = () => {
           </button>
         </div>
 
-        <motion.div
-          initial={false}
-          animate={{ x: open ? 0 : '-100%' }}
-          exit={{ x: '-100%', transition: { duration: 1} }} 
-          transition={{ duration: 0.3,ease:'easeOut'  }}
+        <div
           className="items-center justify-between hidden w-full  md:flex md:w-auto md:order-1"
           id="navbar-sticky"
         >
           <ul
-            className="flex flex-col p-4 md:p-0 mt-4 font-medium border  rounded-lg bg-[#0f0f0f] md:space-x-6 rtl:space-x-reverse md:flex-row md:mt-0  py-4  "
+            className="flex flex-col p-4 md:p-0 mt-4 font-medium border  rounded-lg bg-[#0f0f0f] md:space-x-6 rtl:space-x-reverse md:flex-row md:mt-0 w-[60vw] lg:w-[68vw] items-center justify-center   py-4  "
             style={{ border: "3px solid #333333" }}
           >
             <li>
               <Link
                 href="#"
-                className="block py-2 px-4 mx-3 my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl transition ease-out duration-100  hover:scale-110 hover:px-5"
+                className="block py-1 px-2 mx-1 my-1  lg:py-2 lg:px-4 lg:mx-3 lg:my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl transition ease-out duration-100  hover:scale-110 hover:px-5"
               >
                 Home
               </Link>
@@ -74,7 +71,7 @@ const Navbar = () => {
             <li>
               <Link
                 href="#"
-                className="block py-2 px-4 mx-3 my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl  transition ease-out duration-100  hover:scale-110 "
+                className="block py-0 px-0 mx-0 my-0  lg:py-2 lg:px-4 lg:mx-3 lg:my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl  transition ease-out duration-100  hover:scale-110 "
               >
                 Upcoming
               </Link>
@@ -82,7 +79,7 @@ const Navbar = () => {
             <li>
               <Link
                 href="#"
-                className="block py-2 px-4 mx-3 my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl transition ease-out duration-100  hover:scale-110 "
+                className="block py-0 px-0 mx-0 my-0  lg:py-2 lg:px-4 lg:mx-3 lg:my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl transition ease-out duration-100  hover:scale-110 "
               >
                 Latest
               </Link>
@@ -90,7 +87,7 @@ const Navbar = () => {
             <li>
               <Link
                 href="#"
-                className="block py-2 px-4 mx-3 my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl transition ease-out duration-100  hover:scale-110 "
+                className="block py-0 px-0 mx-0 my-0  lg:py-2 lg:px-4 lg:mx-3 lg:my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl transition ease-out duration-100  hover:scale-110 "
               >
                 Popular
               </Link>
@@ -98,29 +95,28 @@ const Navbar = () => {
             <li>
               <Link
                 href="#"
-                className="block py-2 px-4 mx-3 my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl transition ease-out duration-100  hover:scale-110 "
+                className="block py-0 px-0 mx-0 my-0  lg:py-2 lg:px-4 lg:mx-3 lg:my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl transition ease-out duration-100  hover:scale-110 "
               >
                 Top Rated
               </Link>
             </li>
           </ul>
-        </motion.div>
+        </div>
         <AnimatePresence>
-        {open && (
-          <>
+          {open && (
             <motion.div
-              initial={{ x: '-100%' }}
+              initial={{ x: "-100%" }}
               animate={{ x: 0 }}
-              exit={{ x: '-100%', transition: { duration: 1 } }} 
-              transition={{ duration: 0.3,ease:'easeOut' }}
-              className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000000] bg-[#0f0f0f]`}
+              exit={{ x: "-100%" }}
+              transition={{ duration: 1, ease: "easeOut" }}
+              className={` fixed top-0 left-0 right-0 bottom-0 z-[1000000000] bg-[#0f0f0f]`}
             >
-              <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center ">
-                <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium    bg-[#0f0f0f] md:space-x-6 rtl:space-x-reverse md:flex-row md:mt-0  py-4  ">
+              <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center z-[1000000000]">
+                <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium z-[1000000000]   bg-[#0f0f0f] md:space-x-6 rtl:space-x-reverse md:flex-row md:mt-0  py-4  ">
                   <li>
                     <Link
                       href="#"
-                      className="block w-[80vw] text-center py-2 px-4 mx-3 my-3 text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl transition ease-out duration-100  hover:scale-110 hover:px-5"
+                      className="block w-[80vw] text-center py-2 px-4 mx-3 my-3  text-gray-300 bg-[#0f0f0f] hover:bg-[#1A1A1A] hover:text-white hover:rounded-xl transition ease-out duration-100  hover:scale-110 hover:px-5"
                     >
                       Home
                     </Link>
@@ -159,19 +155,16 @@ const Navbar = () => {
                   </li>
                 </ul>
               </div>
-
-              <motion.button
+              <button
                 onClick={() => {
                   setOpen(false);
                 }}
-                whileTap={{ scale: 0.9 }}
                 className="absolute cursor-pointer top-[1rem] right-[2rem] w-[2rem] h-[2rem] text-white z-[1000000]"
               >
                 <CloseOutlined />
-              </motion.button>
-            </motion.div>
-          </>
-        )}
+              </button>
+              </motion.div>
+          )}
         </AnimatePresence>
       </div>
     </nav>
